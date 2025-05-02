dac09710270006737cc9a8503b5936d1924877ea
Marina <149607434+MariLia5@users.noreply.github.com>
2025-05-02 10:18:22 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE Academy;
+
+USE Academy;
+
+CREATE TABLE curators(
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[name] NVARCHAR (MAX) NOT NULL,
+surname NVARCHAR (MAX) NOT NULL,
+
+CONSTRAINT CHK_curators_name CHECK ([name] NOT LIKE '/\n\n/g'),
+CONSTRAINT CHK_curators_surname CHECK ([name] NOT LIKE '/\n\n/g'),
+);
+
+GO
+
+CREATE TABLE faculties (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+financing MONEY NOT NULL DEFAULT (0),
+[name] NVARCHAR(100) NOT NULL UNIQUE,
+    
+CONSTRAINT CHK_faculties_financing CHECK (financing >= 0),
+CONSTRAINT CHK_faculties_name CHECK ([name] NOT LIKE '/\n\n/g')
+);
+
+GO
+
+CREATE TABLE departments (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+financing MONEY NOT NULL DEFAULT (0),
+[name] NVARCHAR (100) NOT NULL UNIQUE,
+facultyId INT NOT NULL,
+
+CONSTRAINT CHK_departments_financing CHECK (financing >= 0),
+CONSTRAINT CHK_departments_name CHECK ([name] NOT LIKE '/\n\n/g'),
+
+FOREIGN KEY (facultyId)  REFERENCES faculties (id)
+);
+
+GO
+
+CREATE TABLE groups (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[name] NVARCHAR (10) NOT NULL UNIQUE,
+[year] INT NOT NULL,
+departmentId INT NOT NULL,
+
+CONSTRAINT CHK_groups_name CHECK ([name] NOT LIKE '/\n\n/g'),
+CONSTRAINT CHK_groups_year CHECK ([year] >= 1 AND [year] <= 5),
+FOREIGN KEY (departmentId)  REFERENCES departments (id)
+);
+
+GO 
+
+CREATE TABLE  groupsCurators(
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+curatorId INT NOT NULL,
+groupId INT NOT NULL,
+
+FOREIGN KEY (curatorId)  REFERENCES curators (id),
+FOREIGN KEY (groupId)  REFERENCES groups (id)
+);
+
+GO
+
+CREATE TABLE subjects (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[name] NVARCHAR (100) NOT NULL UNIQUE,
+
+CONSTRAINT CHK_subjects_name CHECK ([name] NOT LIKE '/\n\n/g')
+);
+
+GO
+
+CREATE TABLE teachers (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+[name] NVARCHAR (MAX) NOT NULL,
+salary MONEY NOT NULL,
+surname NVARCHAR (MAX) NOT NULL,
+
+CONSTRAINT CHK_teachers_name CHECK ([name] NOT LIKE '/\n\n/g'),
+CONSTRAINT CHK_teachers_salary CHECK (salary > 0),
+CONSTRAINT CHK_teachers_surname CHECK (surname NOT LIKE '/\n\n/g')
+);
+
+GO
+
+CREATE TABLE lectures (
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+lectureRoom NVARCHAR (MAX) NOT NULL,
+subjectId INT NOT NULL,
+teacherId INT NOT NULL,
+
+CONSTRAINT CHK_lectures_lectureRoom CHECK (lectureRoom NOT LIKE '/\n\n/g'),
+
+FOREIGN KEY (subjectId)  REFERENCES subjects (id),
+FOREIGN KEY (teacherId)  REFERENCES teachers (id)
+);
+
+
+CREATE TABLE groupsLectures(
+id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+groupId INT NOT NULL,
+lectureId INT NOT NULL,
+
+FOREIGN KEY (groupId)  REFERENCES groups (id),
+FOREIGN KEY (lectureId)  REFERENCES lectures (id)
+);
+
+--DROP DATABASE Academy;
+
+INSERT INTO curators ([name],surname) VALUES
+('Северус', 'Снейп')
+('Альбус', 'Дамболдор')
+SELECT * FROM curators;
+
+INSERT INTO faculties (financing, [name]) VALUES
+(1000, 'Зельеварение'),
+(2000, 'Темная магия'),
+(3000, 'Защита от темных сил')
+SELECT * FROM faculties;
+
+UPDATE curators
+SET [name] = 'Альбус-Северус'
+WHERE id = 1;
+SELECT * FROM curators;
+
+UPDATE faculties
+SET [name] = 'Предвиденье'
+WHERE financing > 1000;
+SELECT * FROM faculties;
+
+DELETE curators 
+WHERE id = 1;
+SELECT * FROM curators;
+
+DELETE faculties
+WHERE financing > 1000;
+SELECT * FROM faculties;
+
+-- DROP DATABASE Academy;
